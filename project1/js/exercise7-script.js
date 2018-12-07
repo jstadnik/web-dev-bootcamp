2c8425b8201e09c5f34bff016eb3d70e17ca77fe
Joanna Stadnik <j.stadnik123@gmail.com>
2018-12-07 23:32:31 +0000

    Updated exercise 7

--- project1/js/exercise7-script.js
+++ project1/js/exercise7-script.js
@@ -5,11 +5,27 @@ function addItem()
 {
     itemToAdd = prompt("What would you like to add?")
     todoList.push(itemToAdd);
+    console.log("Item: \"" + itemToAdd + "\" has been added to the list");
+}
+
+function printItem(itemName, index)
+{
+    console.log((index+1) + ": " + itemName);
 }
 
 function display()
 {
-    console.log(todoList)
+    console.log("*****************")
+    todoList.forEach(printItem);
+    console.log("*****************")
+}
+
+function deleteItem()
+{
+    var indexToDelete = prompt("Which item would you like to delete?");
+    indexToDelete = Number(indexToDelete)
+    var deletedName = todoList.splice(indexToDelete-1, 1);
+    console.log("Item: \"" + deletedName + "\" has been removed from the list");
 }
 
 while (action !== "quit")
@@ -23,8 +39,13 @@ while (action !== "quit")
     {
         display();
     }
+    else if (action === "delete")
+    {
+        deleteItem();
+    }
     else if (action === "quit")
     {
+        console.log("You have quit the app");
         continue;
     }
     else
